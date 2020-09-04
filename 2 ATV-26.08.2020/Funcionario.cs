a217403de20334a36cbf30eb231ed19c344566d6
StephNascimento <69490116+StephNascimento@users.noreply.github.com>
2020-09-04 15:36:14 -0300

    Add files via upload
    Alteração do código, tentei trazer o método da outra classe, mas creio que foi ignorado

---
+++ 2 ATV-26.08.2020/Funcionario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_ATV2_26._08._2020
+{
+    class Funcionario
+    {
+        public string nome;
+        public double salario;
+
+        public double Salario(Funcionario A, Funcionario B)
+        {
+            double M = (A.salario + B.salario) / 2;
+            return M;
+        }
+    }
+}
---
+++ 2 ATV-26.08.2020/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace POO_ATV2_26._08._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario A, B;
+            A = new Funcionario();
+            B = new Funcionario();
+
+            Console.WriteLine("Escreva seu nome e salário");
+            Console.Write("Nome: ");
+            A.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            A.salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escreva seu nome e salário");
+            Console.Write("Nome: ");
+            B.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            B.salario = double.Parse(Console.ReadLine());                      
+
+            Double M = (A.salario + B.salario) / 2;
+            Console.WriteLine("Média salarial: " + M);
+
+            if(A.salario > B.salario)
+            {
+                Console.WriteLine("Salário maior: " + A.nome);
+            }
+            else
+            {
+                Console.WriteLine("Salário maior: " + B.nome);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

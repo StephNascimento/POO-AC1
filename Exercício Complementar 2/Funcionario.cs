c5cf644262daffd61ad6e3ab24c8111f43a59342
StephNascimento <69490116+StephNascimento@users.noreply.github.com>
2020-09-08 20:53:35 -0300

    Add files via upload
    Exercício com salário bruto, salário liquído, imposto e porcentagem

---
+++ Exercício Complementar 2/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATV_COM_2_07._09._2020
+{
+    public class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            double valor = SalarioBruto - Imposto;
+            return valor;
+        }
+
+        public double AumentarSalario(double porcentagem)
+        {
+            double valor = (SalarioBruto * porcentagem) / 100;
+            return valor;
+        }
+    }
+}
---
+++ Exercício Complementar 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace ATV_COM_2_07._09._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario a = new Funcionario();
+
+            Console.Write("Informe seu nome: ");
+            a.Nome = Console.ReadLine();
+            Console.Write("Informe seu salário: ");
+            a.SalarioBruto = double.Parse(Console.ReadLine());
+            Console.Write("Informe o valor do imposto: ");
+            a.Imposto = double.Parse(Console.ReadLine());
+
+            double L = a.SalarioLiquido();
+            Console.WriteLine("Funcionario: " + a.Nome + ", Salário: " + L.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Porcentagem de aumento?");
+            double p = a.AumentarSalario(double.Parse(Console.ReadLine()));
+            L = L + p;
+            Console.WriteLine("Dados atualizados: " + a.Nome + ", R$" + L.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}

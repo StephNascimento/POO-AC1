39ea1132e168b8ebd43e991c1a7c473b689a10a5
StephNascimento <69490116+StephNascimento@users.noreply.github.com>
2020-09-08 17:58:25 -0300

    Add files via upload
    Exercício com cálculo da área, perímetro e diagonal do retângulo

---
+++ Exercício Complementar 1/Medidas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATV_COM_07._09._2020
+{
+    public class Medidas
+    {
+        public double altura;
+        public double largura;
+    }
+}
---
+++ Exercício Complementar 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace ATV_COM_07._09._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Medidas x = new Medidas();
+
+            Console.Write("Coloque a altura do retângulo: ");
+            x.altura = double.Parse(Console.ReadLine());
+            Console.Write("Coloque a largura do retângulo: ");
+            x.largura = double.Parse(Console.ReadLine());
+
+            double d = (x.altura * x.altura) + (x.largura * x.largura);
+            d = Math.Sqrt(d);
+
+            Console.WriteLine("Área: " + x.altura * x.largura);
+            Console.WriteLine("Perímetro: " + ((2 * x.altura) + (2 * x.largura)));
+            Console.WriteLine("Diagonal: " + d);
+
+            Console.ReadKey();
+        }
+    }
+}

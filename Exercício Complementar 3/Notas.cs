34be1ab484cfb551b9ed22af8a8bac9bc8ce30ae
StephNascimento <69490116+StephNascimento@users.noreply.github.com>
2020-09-08 21:16:05 -0300

    Add files via upload
    Exercício com todas as notas, mostrando se está aprovado ou não

---
+++ Exercício Complementar 3/Notas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATV_COM3_07._09._2020
+{
+    class Notas
+    {
+        public string Nome;
+        public int Nota1;
+        public int Nota2;
+        public int Nota3;
+
+        public int SomaNotas()
+        {
+            int valor = Nota1 + Nota2 + Nota3;
+            return valor;
+        }
+
+        public int Reprova()
+        {
+            int valor = Nota1 + Nota2 + Nota3;
+            valor = 60 - valor;
+            return valor;
+        }
+    }
+}
---
+++ Exercício Complementar 3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ATV_COM3_07._09._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notas a = new Notas();
+
+            Console.Write("Nome do aluno: ");
+            a.Nome = Console.ReadLine();
+            Console.Write("Qual a primeira nota do aluno? ");
+            a.Nota1 = int.Parse(Console.ReadLine());
+            Console.Write("Qual a segunda nota do aluno? ");
+            a.Nota2 = int.Parse(Console.ReadLine());
+            Console.Write("Qual a terceira nota do aluno? ");
+            a.Nota3 = int.Parse(Console.ReadLine());
+
+            int final = a.SomaNotas();
+            int r = a.Reprova();
+            Console.WriteLine("Nota Final: " + final);
+
+            if (final > 60)
+            {
+                Console.WriteLine("APROVADO!");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO!");
+                Console.WriteLine("Faltaram " + r + " pontos");
+            }
+            Console.ReadKey();
+        }
+    }
+}

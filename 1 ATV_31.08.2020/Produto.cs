b1eadc9bf0c380498d2fd0ca814098d00d536371
StephNascimento <69490116+StephNascimento@users.noreply.github.com>
2020-09-09 22:07:51 -0300

    Add files via upload
    Exercício do dia 31/08/2020

---
+++ 1 ATV_31.08.2020/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATV_EXER1_31._08._2020
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;        
+      
+        public void Adicionar(int quan)
+        {
+            Quantidade = Quantidade + quan;
+        }
+
+        public void Remover(int quan)
+        {
+            Quantidade = Quantidade - quan;
+        }
+
+        public double ValorTotal()
+        {
+            double valor = Preco * Quantidade;
+            return valor;
+        }
+    }
+}
---
+++ 1 ATV_31.08.2020/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace ATV_EXER1_31._08._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto a = new Produto();
+
+            Console.WriteLine("Entre com os produtos: ");
+            Console.Write("Nome: ");
+            a.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            a.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade: ");
+            a.Quantidade = int.Parse(Console.ReadLine());
+            double totalestoque = a.ValorTotal();
+
+            Console.WriteLine("Dados do produto: " + a.Nome + ", R$" + a.Preco + ", " + a.Quantidade +
+                ", unidades, total: R$" + totalestoque.ToString("F2", CultureInfo.InvariantCulture));
+            Console.Write("Digite o número a ser adicionado no estoque: ");
+            a.Adicionar(int.Parse(Console.ReadLine()));
+            totalestoque = a.ValorTotal();
+            Console.WriteLine("Dados atualizados: " + a.Nome + ", R$" + a.Preco + ", " + a.Quantidade +
+                " unidades, total: R$" + totalestoque.ToString("F2", CultureInfo.InvariantCulture));
+            Console.Write("Digite o número a ser removido no estoque: ");
+            a.Remover(int.Parse(Console.ReadLine()));
+            totalestoque = a.ValorTotal();
+
+            Console.WriteLine("Dados atualizados: " + a.Nome + ", R$" + a.Preco + ", " + a.Quantidade +
+                " unidades, total: R$" + totalestoque.ToString("F2", CultureInfo.InvariantCulture)) ;
+            Console.ReadKey();
+        }
+    }
+}
